f843b7b983e9323ff0283a6611f77e2cf3464759
carolinaguercio <caroguercio@gmail.com>
2019-06-27 23:17:50 -0300

    creo archivos compactacion

---
+++ LFS/LFS_Compactacion.c
@@ -0,0 +1,6 @@
+/*
+ * LFS_Compactacion.c
+ *
+ *  Created on: 27 jun. 2019
+ *      Author: utnso
+ */
---
+++ LFS/LFS_Compactacion.h
@@ -0,0 +1,15 @@
+/*
+ * LFS_Compactacion.h
+ *
+ *  Created on: 27 jun. 2019
+ *      Author: utnso
+ */
+
+#ifndef LFS_COMPACTACION_H_
+#define LFS_COMPACTACION_H_
+
+
+
+
+
+#endif /* LFS_COMPACTACION_H_ */
