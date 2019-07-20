8d7cd461bd7745bdd952c9e9aafe8b5b381b8116
Melina Borque <Melinaborque@hotmail.com>
2019-07-20 13:37:28 -0300

    Hay un FREE de mas en Memoria

--- Kernel/Kernel.c
+++ Kernel/Kernel.c
@@ -125,12 +125,13 @@ void* iniciar_peticion_tablas(void* memorias_disponibles){
 
 			int socket_memoria_a_pedir = crear_conexion(memoria_a_pedir->ip,memoria_a_pedir->puerto);
 
-			if(socket_memoria_a_pedir != -1){
-				close(socket_memoria);
+			if(socket_memoria_a_pedir > 0){
+//				close(socket_memoria);
 				send(socket_memoria_a_pedir,&operacion,sizeof(t_operacion),MSG_WAITALL);
 				recibir_tabla_de_gossiping(socket_memoria_a_pedir);
 				log_info(logger, "FIN PEDIDO DE TABLAS");
-				socket_memoria = socket_memoria_a_pedir;
+				liberar_conexion(socket_memoria_a_pedir);
+//				socket_memoria = socket_memoria_a_pedir;
 				i = tamanio;
 			} else {
 				i++;
@@ -150,8 +151,7 @@ int socket_memoria_principal(){
 		pthread_mutex_unlock(&memorias_disponibles_mutex);
 
 		int socket_memoria_a_pedir = crear_conexion(memoria_a_pedir->ip,memoria_a_pedir->puerto);
-		if(socket_memoria_a_pedir != -1){
-			close(socket_memoria);
+		if(socket_memoria_a_pedir > 0){
 			return socket_memoria_a_pedir;
 		}
 		i++;
@@ -314,17 +314,20 @@ void resolver_describe_drop(t_instruccion_lql instruccion, t_operacion operacion
 	paquete_describe->codigo_operacion=operacion;
 
 	char* nombre_tabla = paquete_describe->nombre_tabla;
-	int socket_memoria_a_usar = socket_memoria;//conseguir_memoria(nombre_tabla, -1);
-
-	enviar_paquete_drop_describe(socket_memoria_a_usar, paquete_describe);
-
-	t_status_solicitud* status = desearilizar_status_solicitud(socket_memoria_a_usar);
-	if(status->es_valido){
-		log_info(logger, "Resultado: %s", status->mensaje->palabra);
+	int socket_memoria_a_usar = socket_memoria_principal();//socket_memoria;//conseguir_memoria(nombre_tabla, -1);
+	if(socket_memoria_a_usar == -1){
+			log_error(logger, "No se pudo realizar el DESCRIBE por falta de Memorias disponibles");
 	}else{
-		log_error(logger, "Error: %s", status->mensaje->palabra);
+		enviar_paquete_drop_describe(socket_memoria_a_usar, paquete_describe);
+
+		t_status_solicitud* status = desearilizar_status_solicitud(socket_memoria_a_usar);
+		if(status->es_valido){
+			log_info(logger, "Resultado: %s", status->mensaje->palabra);
+		}else{
+			log_error(logger, "Error: %s", status->mensaje->palabra);
+		}
+		eliminar_paquete_drop_describe(paquete_describe);
 	}
-	eliminar_paquete_drop_describe(paquete_describe);
 }
 
 void resolver_describe(t_instruccion_lql instruccion){
@@ -336,39 +339,40 @@ void resolver_describe(t_instruccion_lql instruccion){
 	int socket_memoria_a_usar = socket_memoria_principal();
 	if(socket_memoria_a_usar == -1){
 		log_error(logger, "No se pudo realizar el DESCRIBE por falta de Memorias disponibles");
-	}
-	enviar_paquete_drop_describe(socket_memoria_a_usar, paquete_describe);
-
-	log_info(logger, "Se realiza DESCRIBE");
-	if(string_is_empty(paquete_describe->nombre_tabla->palabra)){
-		log_info(logger, "Se trata de un describe global.");
-		int cant_tablas= recibir_numero_de_tablas (socket_memoria_a_usar);
-		log_info(logger, "Cantidad de tablas en LFS: %i", cant_tablas);
-		for(int i=0; i<cant_tablas; i++){
-			t_metadata* metadata = deserealizar_metadata(socket_memoria_a_usar);
-			log_info(logger, "Metadata tabla: %s", metadata->nombre_tabla->palabra);
-			log_info(logger, "Consistencia: %s", tipo_consistencia(metadata->consistencia));
-			log_info(logger, "Numero de particiones: %d", metadata->n_particiones);
-			log_info(logger, "Tiempo de compactacion: %d", metadata->tiempo_compactacion);
-
-			guardar_consistencia_tabla(metadata->nombre_tabla->palabra, metadata->consistencia);
-		}
 	}else{
-		t_status_solicitud* status = desearilizar_status_solicitud(socket_memoria_a_usar);
-		if (status->es_valido){
-			t_metadata* t_metadata = deserealizar_metadata(socket_memoria_a_usar);
-			//aca se está mostrando pero deberia guardarselo no?
-			log_info(logger, "Metadata tabla: %s", t_metadata->nombre_tabla->palabra);
-			log_info(logger, "Consistencia: %s", tipo_consistencia(t_metadata->consistencia));
-			log_info(logger, "Numero de particiones: %d", t_metadata->n_particiones);
-			log_info(logger, "Tiempo de compactacion: %d", t_metadata->tiempo_compactacion);
-		}else{
-			log_info(logger, "Error: %s", status->mensaje->palabra);
-		}
+		enviar_paquete_drop_describe(socket_memoria_a_usar, paquete_describe);
 
+		log_info(logger, "Se realiza DESCRIBE");
+		if(string_is_empty(paquete_describe->nombre_tabla->palabra)){
+			log_info(logger, "Se trata de un describe global.");
+			int cant_tablas= recibir_numero_de_tablas (socket_memoria_a_usar);
+			log_info(logger, "Cantidad de tablas en LFS: %i", cant_tablas);
+			for(int i=0; i<cant_tablas; i++){
+				t_metadata* metadata = deserealizar_metadata(socket_memoria_a_usar);
+				log_info(logger, "Metadata tabla: %s", metadata->nombre_tabla->palabra);
+				log_info(logger, "Consistencia: %s", tipo_consistencia(metadata->consistencia));
+				log_info(logger, "Numero de particiones: %d", metadata->n_particiones);
+				log_info(logger, "Tiempo de compactacion: %d", metadata->tiempo_compactacion);
+
+				guardar_consistencia_tabla(metadata->nombre_tabla->palabra, metadata->consistencia);
+			}
+		}else{
+			t_status_solicitud* status = desearilizar_status_solicitud(socket_memoria_a_usar);
+			if (status->es_valido){
+				t_metadata* t_metadata = deserealizar_metadata(socket_memoria_a_usar);
+				//aca se está mostrando pero deberia guardarselo no?
+				log_info(logger, "Metadata tabla: %s", t_metadata->nombre_tabla->palabra);
+				log_info(logger, "Consistencia: %s", tipo_consistencia(t_metadata->consistencia));
+				log_info(logger, "Numero de particiones: %d", t_metadata->n_particiones);
+				log_info(logger, "Tiempo de compactacion: %d", t_metadata->tiempo_compactacion);
+			}else{
+				log_info(logger, "Error: %s", status->mensaje->palabra);
+			}
+
+		}
+		eliminar_paquete_drop_describe(paquete_describe);
+		liberar_conexion(socket_memoria_a_usar);
 	}
-	eliminar_paquete_drop_describe(paquete_describe);
-	liberar_conexion(socket_memoria_a_usar);
 }
 
 void guardar_consistencia_tabla(char* nombre_tabla, t_consistencia consistencia){
