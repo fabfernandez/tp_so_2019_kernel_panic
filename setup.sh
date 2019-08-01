803686607dadd7db5e28e3bc47ac970c5e22e236
aa <a@a>
2019-08-01 17:36:32 -0300

    El setup esta listo para la entrega final

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-#echo "Se instala Commons"
-#cd ..
-#git clone https://github.com/sisoputnfrba/1C2019-Scripts-lql-entrega.git
-#git clone https://github.com/sisoputnfrba/so-commons-library
-#cd so-commons-library
-#sudo make install
-#cd ..
+echo "Se instala el repo de Configs"
+cd..
+git clone https://github.com/chaloxc/FINAL.git
+echo "Se instala Commons"
+git clone https://github.com/sisoputnfrba/1C2019-Scripts-lql-entrega.git
+git clone https://github.com/sisoputnfrba/so-commons-library
+cd so-commons-library
+sudo make install
+cd ..
 echo "Compilacion de proyecto"
-#cd tp-2019-1c-Los-Dinosaurios-Del-Libro
+cd tp-2019-1c-Los-Dinosaurios-Del-Libro
 cd global/Debug/
 make clean
 make all
